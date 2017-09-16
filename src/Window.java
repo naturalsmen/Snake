0c52296b2a1186d2cc3dc11d013029f703a30dc2
Eric Hung <naturalsmen@gmail.com>
2017-09-16 15:40:51 +0800

    Create new class "Menu"
    New class "Menu" is created to allow users to choose different
    difficulties, simple or hard.

--- src/Window.java
+++ src/Window.java
@@ -1,7 +1,11 @@
-import java.awt.GridLayout;
+import java.awt.*;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 
@@ -53,5 +57,62 @@ class Window extends JFrame{
 		//ControlleurThreads c2 = new ControlleurThreads(position2);
 		//c2.start();
 		
+	}
+}
+
+class Menu extends JFrame{
+	private static final long serialVersionUID = 1L;
+	
+	public JButton Simple = new JButton("Simple");
+	public JButton Hard = new JButton("Hard");
+	
+	//Simple Mode -> speed = 70
+	//Hard Mode -> speed = 60
+	public Menu() {
+	  getContentPane().setLayout(new FlowLayout());
+	  getContentPane().add(Simple);
+	  getContentPane().add(Hard);
+
+		Simple.addActionListener(new ActionListener() {
+
+          public void actionPerformed(ActionEvent e) {
+          	ThreadsController.speed = 70;
+          	ThreadsController.level = 1;
+        	Window f1= new Window();
+          	f1.setTitle("Snake");
+      		f1.setSize(600,600);    //300 -> 600
+      		f1.setVisible(true);
+      		f1.setLocationRelativeTo(null);
+      		f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      		
+      		//Close Menu once select Simple
+      		String cmd = e.getActionCommand();
+      		if(cmd.equals("Simple"))
+            {
+                dispose();
+            }
+          };
+      });
+		
+		Hard.addActionListener(new ActionListener() {
+
+	          public void actionPerformed(ActionEvent e) {                 
+	          	ThreadsController.speed = 50;
+	          	ThreadsController.level = 2;
+	        	Window f1= new Window();
+	          	f1.setTitle("Snake");
+	      		f1.setSize(600,600);    //300 -> 600
+	      		f1.setVisible(true);
+	      		f1.setLocationRelativeTo(null);
+	      		f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	      		
+	      		//Close Menu once select Hard
+	      		String cmd = e.getActionCommand();
+	      		if(cmd.equals("Hard"))
+	            {
+	                dispose();
+	            }
+	          };
+	      });  
 	}
 }
