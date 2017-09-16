87a3d4932f467f410228072f2c219964f3a5d51d
Eric Hung <naturalsmen@gmail.com>
2017-09-16 15:29:22 +0800

    Call Menu to show difficulty level
    Adjust JFrame to fit the list.

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,13 @@ public class Main {
 	public static void main(String[] args) {
 
 		//Creating the window with all its awesome snaky features
-		Window f1= new Window();
+		Menu f1= new Menu();
 		
 		//Setting up the window settings
 		f1.setTitle("Snake");
-		f1.setSize(300,300);
+		f1.setSize(300,80);
+		f1.setLocationRelativeTo(null);
 		f1.setVisible(true);
-		f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);             
-
+		f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
